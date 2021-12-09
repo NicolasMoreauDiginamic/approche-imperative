bc6812f789211353f18921a3b130b4e02dce280d
Nicolas Moreau <nicolas.moreau93@orange.fr>
2021-12-09 14:33:01 +0100

    commit de la classe DeclarationApp

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,30 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// variables
+		byte nombre1 = 2;
+		short nombre2 = 4;
+		int nombre3 = 6;
+		long nombre4 = 8;
+		float nombre5 = 5.3f;
+		double nombre6 = 4.6d;
+		char carac = 'r';
+		boolean bool = true;
+		String chaine = "Test";
+		String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		
+		System.out.println(nombre1);
+		System.out.println(nombre2);
+		System.out.println(nombre3);
+		System.out.println(nombre4);
+		System.out.println(nombre5);
+		System.out.println(nombre6);
+		System.out.println(carac);
+		System.out.println(bool);
+		System.out.println(chaine);
+		System.out.println(randomString);
+	}
+
+}
